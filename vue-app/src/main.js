56a0dfbcf2d6f4ebc52aa8776a5871b1bc76365b
Prajjwal Sharma <Prajjwal98@google.com>
2026-08-09 15:21:14 +0530

    Refactor to Zero-Install CDN Vue architecture

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+const { loadModule } = window['vue3-sfc-loader'];
 
-createApp(App).mount('#app')
+const options = {
+  moduleCache: {
+    vue: Vue
+  },
+  async getFile(url) {
+    const res = await fetch(url);
+    if (!res.ok)
+      throw Object.assign(new Error(res.statusText + ' ' + url), { res });
+    return {
+      getContentData: asBinary => asBinary ? res.arrayBuffer() : res.text(),
+    }
+  },
+  addStyle(textContent) {
+    const style = Object.assign(document.createElement('style'), { textContent });
+    const ref = document.head.getElementsByTagName('style')[0] || null;
+    document.head.insertBefore(style, ref);
+  },
+};
+
+const app = Vue.createApp({
+  components: {
+    'app-root': Vue.defineAsyncComponent(() => loadModule('./src/App.vue', options))
+  },
+  template: '<app-root></app-root>'
+});
+
+app.mount('#app');
